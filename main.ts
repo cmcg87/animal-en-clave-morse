e389ff94bcee4b34de21aeb2ff7505b91d356d0b
cmcg87 <83800381+cmcg87@users.noreply.github.com>
2021-05-06 22:39:54 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,80 @@
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
